7989464f8b2cb86173ac15bd4a76ee65ad6d5223
nabixushiwole <82488197+Feng3333@users.noreply.github.com>
2024-09-03 19:41:54 +0800

    Update password_encode.py
    【FE/RR】US202409031941
    【Modification】Update password_encode.py
    【Modifier】Feng3333
    【Reviewer】Feng3333

--- MyFreeProgram/cryptography_process/password_processing/password_encode.py
+++ MyFreeProgram/cryptography_process/password_processing/password_encode.py
@@ -3,7 +3,20 @@
 # Create: 2024-08-26
 #
 
-from .password_encode_dict import morse_encode_dict
+from .password_encode_dict import morse_encode_dict, keyboard_to_letters_encode_list, phone_nine_box_encode_dict
+
+
+def generate_final_processed_code(codes: list):
+    """
+    生成最终的密码结果(字符串形式)
+    :param codes: str
+    :return: str
+    """
+    res_str = str()
+    for code in codes:
+        res_str += code
+        res_str += " "
+    return res_str
 
 
 def encode_to_morse_code(letters: str):
@@ -18,9 +31,29 @@ def encode_to_morse_code(letters: str):
     return morse_code_list
 
 
-def generate_final_processed_code(codes: list):
-    res_str = str()
-    for code in codes:
-        res_str += code
-        res_str += " "
-    return res_str
+def keyboard_to_letters_encode(str_list: list) -> list:
+    """
+    # 字母转键盘字母QWE加密法 ABC=QWE
+    :param str_list: list
+    :return: list
+    """
+    res_list = []
+    for item in str_list:
+        res_list.append(keyboard_to_letters_encode_list[item])
+    return res_list
+
+
+def phone_nine_box_encode(str_list: list) -> list:
+    """
+    # 字母转手机九宫格
+    :param str_list: list
+    :return:
+    """
+    res_list = []
+    for item in str_list:
+        if not item.isalpha():
+            print('This str list is not all letter')
+            return []
+        else :
+            res_list.append(phone_nine_box_encode_dict[item])
+    return res_list
