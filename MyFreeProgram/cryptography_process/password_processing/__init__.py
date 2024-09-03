3ed443b49b55a975381b061f0dcaeded246afd09
nabixushiwole <82488197+Feng3333@users.noreply.github.com>
2024-09-03 19:42:39 +0800

    Update __init__.py
    【FE/RR】US202409031942
    【Modification】Update __init__.py
    【Modifier】Feng3333
    【Reviewer】Feng3333

--- MyFreeProgram/cryptography_process/password_processing/__init__.py
+++ MyFreeProgram/cryptography_process/password_processing/__init__.py
@@ -4,4 +4,4 @@
 #
 
 from .password_decode_dict import morse_decode_dict, phone_nine_box_decode_list, keyboard_to_letters_decode_list
-from .password_encode_dict import morse_encode_dict, keyboard_to_letters_encode_list
+from .password_encode_dict import morse_encode_dict, keyboard_to_letters_encode_list, phone_nine_box_encode_dict
