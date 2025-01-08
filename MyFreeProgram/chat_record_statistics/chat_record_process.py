9e90b00633749a3088a8f4dbf38c60e6f3d328b4
nabixushiwole <82488197+Feng3333@users.noreply.github.com>
2025-01-08 09:49:43 +0800

    Update chat_record_process.py
    【FE/RR】FE202501080949
    【Modification】Update chat_record_process.py
    【Modifier】Feng3333
    【Reviewer】Feng3333

--- MyFreeProgram/chat_record_statistics/chat_record_process.py
+++ MyFreeProgram/chat_record_statistics/chat_record_process.py
@@ -1,1 +1,7 @@
+import os
+from chat_record_tools import chat_record_statistic_process
 
+
+if __name__ == '__main__':
+    file_name = os.path.join(r'D:\TempFile\HistoryRecord\chatting.txt')
+    chat_record_statistic_process(file_name)
