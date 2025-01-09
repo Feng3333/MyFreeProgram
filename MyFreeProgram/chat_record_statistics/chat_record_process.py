7578a0dd69edfee6f7126710551c8c80590dff5c
nabixushiwole <82488197+Feng3333@users.noreply.github.com>
2025-01-09 16:26:56 +0800

    Update chat_record_process.py
    【FE/RR】US202501091626
    【Modification】Update chat_record_process.py
    【Modifier】Feng3333
    【Reviewer】Feng3333

--- MyFreeProgram/chat_record_statistics/chat_record_process.py
+++ MyFreeProgram/chat_record_statistics/chat_record_process.py
@@ -1,7 +1,16 @@
+#
+# Description: Program Entry
+# Create: 2025-01-09
+#
+
 import os
 from chat_record_tools import chat_record_statistic_process
+from chat_text_summary import chat_content_process
 
 
 if __name__ == '__main__':
     file_name = os.path.join(r'D:\TempFile\HistoryRecord\chatting.txt')
-    chat_record_statistic_process(file_name)
+    front_part = os.path.dirname(file_name)
+    # 1. chat_record_statistic_process
+    person_info_list, person_chat_contents, all_chat_contents = chat_record_statistic_process(file_name, front_part)
+    chat_content_process(person_info_list, person_chat_contents, all_chat_contents, front_part)
