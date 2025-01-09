8386d98af0551a4f32e0e02b23622b074b0f59f6
nabixushiwole <82488197+Feng3333@users.noreply.github.com>
2025-01-09 17:16:23 +0800

    Update chat_record_process.py
    【FE/RR】US202501091716
    【Modification】Update chat_record_process.py
    【Modifier】Feng3333
    【Reviewer】Feng3333

--- MyFreeProgram/chat_record_statistics/chat_record_process.py
+++ MyFreeProgram/chat_record_statistics/chat_record_process.py
@@ -9,8 +9,13 @@ from chat_text_summary import chat_content_process
 
 
 if __name__ == '__main__':
-    file_name = os.path.join(r'D:\TempFile\HistoryRecord\chatting.txt')
+    # 1. file path process
+    file_name = os.path.join(r'D:\TempFile\HistoryRecord\推协DM性能倍增特战队(687123446244684496).txt')
     front_part = os.path.dirname(file_name)
-    # 1. chat_record_statistic_process
-    person_info_list, person_chat_contents, all_chat_contents = chat_record_statistic_process(file_name, front_part)
-    chat_content_process(person_info_list, person_chat_contents, all_chat_contents, front_part)
+    output_path = os.path.join(front_part, 'chat_record')
+    if not os.path.exists(output_path):
+        os.makedirs(output_path, exist_ok=True)
+    # 2. chat_record_statistic_process
+    person_info_list, person_chat_contents, all_chat_contents = chat_record_statistic_process(file_name, output_path)
+    # 3. 
+    chat_content_process(person_info_list, person_chat_contents, all_chat_contents, output_path)
